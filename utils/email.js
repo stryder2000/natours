9a85efb81688e240d2adccd7e25525a1faf71d3b
stryder2000 <github.com/stryder2000>
2021-02-01 20:53:40 +0530

    Added updated stripe key and implemented google in nodemailer

--- utils/email.js
+++ utils/email.js
@@ -3,68 +3,65 @@ const pug = require('pug');
 const htmlToText = require('html-to-text');
 
 module.exports = class Email {
-    constructor(user, url) {
-        this.from = `Siddharth Singh ${process.env.EMAIL_FROM}`;
-        this.url = url;
-        this.firstname = user.name.split(' ')[0];
-        this.to = user.email;
+  constructor(user, url) {
+    this.from = `Siddharth Singh ${process.env.EMAIL_FROM}`;
+    this.url = url;
+    this.firstname = user.name.split(' ')[0];
+    this.to = user.email;
+  }
+
+  async newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Gmail
+      return nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: process.env.GMAIL_USERNAME,
+          pass: process.env.GMAIL_PASSWORD,
+        },
+      });
     }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-    async newTransport() {
-        if (process.env.NODE_ENV === 'production') {
-            //Sendgrid
-            return nodemailer.createTransport({
-                service: 'SendGrid',
-                auth: {
-                    user: process.env.SENDGRID_USERNAME,
-                    pass: process.env.SENDGRID_PASSWORD
-                }
-            });
-        }
-        return nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
-    }
+  //Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
 
-    //Send the actual email
-    async send(template, subject) {
-        // 1) Render HTML based on a pug template
-        const html = pug.renderFile(
-            `${__dirname}/../views/email/${template}.pug`,
-            {
-                firstName: this.firstName,
-                url: this.url,
-                subject
-            }
-        );
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
 
-        // 2) Define email options
-        const mailOptions = {
-            from: this.from,
-            to: this.to,
-            subject,
-            html,
-            text: htmlToText.fromString(html)
-        };
+    // 3) Create a transport and send email
+    const transporter = await this.newTransport();
+    await transporter.sendMail(mailOptions);
+  }
 
-        // 3) Create a transport and send email
-        const transporter = await this.newTransport();
-        await transporter.sendMail(mailOptions);
-    }
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
 
-    async sendWelcome() {
-        await this.send('welcome', 'Welcome to the Natours Family!');
-    }
-
-    async sendPasswordReset() {
-        await this.send(
-            'passwordReset',
-            'Your password reset token (valid for only 10 minutes.)'
-        );
-    }
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes.)'
+    );
+  }
 };
