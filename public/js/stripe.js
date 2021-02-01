dd1eb23f56b1ab34670174c6c701ebaac5130c25
stryder2000 <github.com/stryder2000>
2021-02-01 18:29:37 +0530

    Stripe issue resolved

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,22 +1,20 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 const stripe = Stripe(
-    'pk_test_51H8TgkGn4dG3D9SdQfijUzjxRbENvm3xxwrzByxg66rzoU4bI5wtd8cdXjbwWlOyIvCXDA5fM95SmHQxp8Q03I7S00Dnd7fjg2'
+  'sk_test_51H8TgkGn4dG3D9SdasKbVTzaH1LGjcrfuagNafcR7tQrItqm7zxNLZbUiwb0xXlj05xOuOi6riuLPI3Vib940MrG00RlUsj2Bn'
 );
 
-export const bookTour = async tourId => {
-    try {
-        // 1) Get checkout session from API
-        const session = await axios(
-            `/api/v1/bookings/checkout-session/${tourId}`
-        );
-//        console.log(session);
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    //        console.log(session);
 
-        // 2) Create checkout form + charge credit card
-        await stripe.redirectToCheckout({
-            sessionId: session.data.session.id
-        });
-    } catch (err) {
-        showAlert('error', err.message);
-    }
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    showAlert('error', err.message);
+  }
 };
